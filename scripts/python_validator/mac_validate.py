08bb5f8c7c0b38baed5319ec2b32374b1ad692a9
Ignacio <ignacio@ignapzs.net>
2023-03-20 22:27:45 +0100

    feat: - specific validation exception

--- scripts/python_validator/mac_validate.py
+++ scripts/python_validator/mac_validate.py
@@ -55,12 +55,17 @@ class YAMLReader(DataReader):
             return yaml.safe_load(fd)
 
 
+class UnknownFormatException(Exception):
+    """Unknown format exception"""
+    pass
+
+
 def reader_builder(filename):
     if filename.lower().endswith("json"):
         return JSONReader(filename)
     if filename.lower().endswith(("yml", "yaml")):
         return YAMLReader(filename)
-    raise Exception("Unknown format")
+    raise UnknownFormatException("Unknown format")
 
 
 def parse_args():
